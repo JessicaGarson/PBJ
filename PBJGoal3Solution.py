d88f36241ad687e53bf3450b43d1223450cdb499
JessicaGarson <jessica.garson@gmail.com>
2014-02-02 11:31:49 -0500

    Update PBJGoal3Solution.py

--- PBJGoal3Solution.py
+++ PBJGoal3Solution.py
@@ -2,8 +2,8 @@ def check(bread_for_num_of_sandwiches):
     if bread_for_num_of_sandwiches%2==0:
         print "You have an evan amount of bread! You should be good for making sandwiches"
     elif bread < 2 and peanut_butter < 1 and jelly < 1: 
-		print "Looks like I don't have lunch today"
-	else:
+		print "Looks like I don't have lunch today" 
+    else:
         print "You will need to make one open faced sandwich"
 
 print "How many slices of bread do you have"
